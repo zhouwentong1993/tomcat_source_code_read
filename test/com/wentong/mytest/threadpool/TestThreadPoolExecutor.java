65c84a318f1c9d3c9ec6106d6b487cee16460d85
zhouwentong <zhouwentong1993@gmail.com>
2022-01-13 15:33:31 +0800

    CPU 密集型任务 Java 自带的要好一些。因为减少 CPU 竞争的开销。

--- test/com/wentong/mytest/threadpool/TestThreadPoolExecutor.java
+++ test/com/wentong/mytest/threadpool/TestThreadPoolExecutor.java
@@ -5,6 +5,9 @@ import org.apache.tomcat.util.threads.TaskThreadFactory;
 import org.apache.tomcat.util.threads.ThreadPoolExecutor;
 import org.junit.Test;
 
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class TestThreadPoolExecutor {
@@ -31,6 +34,40 @@ public class TestThreadPoolExecutor {
         }
         TimeUnit.SECONDS.sleep(10000);
 
+    }
+
+    /**
+     * 测试当 CPU 密集型时，Java 自带的和 Tomcat 的线程池有何不同。
+     */
+    @Test
+    public void testWhenIo() throws Exception {
+        int threadCount = 50;
+
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        CountDownLatch finish = new CountDownLatch(threadCount);
+        long start = System.currentTimeMillis();
+        java.util.concurrent.ThreadPoolExecutor executor = new java.util.concurrent.ThreadPoolExecutor(8, 100, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>(100));
+        for (int i = 0; i < threadCount; i++) {
+            executor.execute(() -> {
+                latch.countDown();
+                long ss = System.currentTimeMillis();
+                long sum = 0;
+                for (int j = 0; j < 100; j++) {
+                    for (int k = 0; k < 1000000; k++) {
+                        int hash = Objects.hashCode(String.valueOf(j + k));
+//                        hash = Objects.hash(hash);
+                        sum += hash;
+                    }
+                }
+                System.out.println("执行时间：" + (System.currentTimeMillis() - ss));
+                System.out.println(sum);
+                System.out.println("-------------------------");
+                finish.countDown();
+            });
+        }
+        latch.await();
+        finish.await();
+        System.out.println("total time is:" + (System.currentTimeMillis() - start));
     }
 
 }
